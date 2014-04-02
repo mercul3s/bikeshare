5f4370b6268224eab161fec2817ac4f879fd6c6b
Mercedes Coyle <mcoyle@ulive.com>
2014-04-01 20:05:17 -0700

    code to pull data from csv to mongodb for analysis

--- data_gather.py
+++ data_gather.py
@@ -1,30 +1,48 @@
 #!/usr/bin/python
 
+from pymongo import MongoClient
 import os
 import csv
 
 path = os.path.dirname("./data/")
+client = MongoClient()
+db = client.bike_share
+
+rebalancing = db.rebalancing
+stations    = db.stations
+trips       = db.trips
+weather     = db.weather
+
+collections = [rebalancing, stations, trips, weather]
 
 def get_data_files(path):
     file_list = os.listdir(path)
-    return file_list
+    csv_files = []
+    for data_file in file_list:
+        if data_file.endswith(".csv") is True:
+            csv_files.append(data_file)
+    return csv_files
 
-def loop_files(files):
-    for data_file in files:
-        if data_file.endswith(".csv"):
-            file_path = os.path.join(path, data_file)
-            print "File path: %s " % file_path
-            with open(file_path, 'r') as csvfile:
-                counter = 0
-                # fileDialect = csv.Sniffer().sniff(csvfile.read(1024), delimiters=",")
-                # csvfile.seek(0)
-                reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-                print dir(reader)
-                # for row in reader: 
-                    # print ', '.join(row)
+def loop_files(filename, collection):
+    file_path = os.path.join(path, filename)
+    with open(file_path, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        header = reader.next()
+        print "Filename: {fn} header: {hd}".format(fn=file_path, hd=header)
+        for row in reader:
+           document = dict(zip(header, row))
+           collection.insert(document)
+           # print document
 
 def main():
     file_list = get_data_files(path)
-    loop_files(file_list)
+    # print file_list
+
+    csv_file_map = dict(zip(file_list, collections))
+    # print csv_file_map
+
+    for csv, coll in csv_file_map.iteritems():
+        loop_files(csv, coll)
+
 if __name__ == '__main__':
     main()
